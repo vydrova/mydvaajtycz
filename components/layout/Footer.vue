528ffc878093f789bcf10dc778dc961296ed8e2f
vydrova <v.janca@gmail.com>
2023-03-05 18:54:37 +0100

    Hrubka

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-md-6">
                     <h2>Kontakt</h2>
-                    <p>V den obřadu se prosím obracejte s jakýmikoliv problémi na naši koordinátorku Katku.</p>
+                    <p>V den obřadu se prosím obracejte s jakýmikoliv problémy na naši koordinátorku Katku.</p>
                     <p>Kateřina Gebhartová: <a href="tel:+420732248379">+420 732 248 379</a></p>
                 </div>
                 <div class="col-md-6">
