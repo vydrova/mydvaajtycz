1dfe98ed61fea7fa6f49d496468728225ce14720
vydrova <v.janca@gmail.com>
2023-03-05 18:53:15 +0100

    target blank

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -10,7 +10,7 @@
                 <div class="col-md-6">
                     <h2>Adresa místa konání</h2>
                     <p>Statek Záryby</p>
-                    <p>Záryby 12<br>277 13<br>GPS: 50.2226422N, 14.6278589E</p>
+                    <p>Záryby 12<br>277 13<br>GPS: <a href="https://mapy.cz/zakladni?x=14.6278589&y=50.2226833&z=17&l=0&source=addr&id=9356382" target="_blank">50.2226422N, 14.6278589E</a></p>
                 </div>
             </div>
         </div>
