3e2d358c4044c8689de9f9da562e257e6f3b366c
vydrova <v.janca@gmail.com>
2023-03-05 18:37:40 +0100

    Oprava textů

--- components/content/Food.vue
+++ components/content/Food.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <h1>Jídlo a pití</h1>
             <p>S jídlem a pitím si nemusíte dělat starosti. Po obřadu se přesuneme ke svatební hostině, která by měla začít okolo 14:00. Dále bude samozřejmě svatební dort a večerní občerstvení ve formě rautu. Pokud máte specifické stravovací potřeby, nahlaste nám je prosím minimálně dva týdny předem.</p>
-            <p>Pití bude k dispozici jak alko, tak nealko. Věříme, že každý si přijde na své.</p>
+            <p>Pití bude k dispozici jak alko, tak nealko. Věříme, že si každý přijde na své.</p>
         </div>
     </section>
 </template>
