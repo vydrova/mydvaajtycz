75601794892cde292d63b220e5e07c08bff91dc7
vydrova <v.janca@gmail.com>
2023-03-06 16:06:12 +0100

    Zmenšení obrázků

--- components/content/Gallery.vue
+++ components/content/Gallery.vue
@@ -14,75 +14,75 @@ export default {
         return {
             data: [
                 {
-                    src: '/img/galerie/2016_blansko.jpeg',
+                    src: '/img/galerie/2016_blansko_670.jpeg',
                     title: 'Blansko 2016'
                 },
                 {
-                    src: '/img/galerie/2017_svycarsko.jpeg',
+                    src: '/img/galerie/2017_svycarsko_670.jpeg',
                     title: 'České Švýcarsko 2017'
                 },
                 {
-                    src: '/img/galerie/2019_jentak.jpg',
+                    src: '/img/galerie/2019_jentak_670.jpg',
                     title: '2019 jen tak'
                 },
                 {
-                    src: '/img/galerie/2019_viden.jpg',
+                    src: '/img/galerie/2019_viden_670.jpg',
                     title: 'Vídeň 2019'
                 },
                 {
-                    src: '/img/galerie/2019_kochofen.jpeg',
+                    src: '/img/galerie/2019_kochofen_670.jpeg',
                     title: 'Kochofen 2019'
                 },
                 {
-                    src: '/img/galerie/2019_vanocnividen.jpg',
+                    src: '/img/galerie/2019_vanocnividen_670.jpg',
                     title: 'Vánoční Vídeň 2019'
                 },
                 {
-                    src: '/img/galerie/2020_babilom.jpg',
+                    src: '/img/galerie/2020_babilom_670.jpg',
                     title: 'Babí lom 2020'
                 },
                 {
-                    src: '/img/galerie/2020_klinovec.jpg',
+                    src: '/img/galerie/2020_klinovec_670.jpg',
                     title: 'Klínovec 2020'
                 },
                 {
-                    src: '/img/galerie/2021_polabi.jpeg',
+                    src: '/img/galerie/2021_polabi_670.jpeg',
                     title: 'Polabí 2021'
                 },
                 {
-                    src: '/img/galerie/2021_vanoce1.jpeg',
+                    src: '/img/galerie/2021_vanoce1_670.jpeg',
                     title: 'Vánoce 2021'
                 },
                 {
-                    src: '/img/galerie/2022_hustopece2.jpg',
+                    src: '/img/galerie/2022_hustopece2_670.jpg',
                     title: 'Hustopeče 2022'
                 },
                 {
-                    src: '/img/galerie/2022_lhota.jpeg',
+                    src: '/img/galerie/2022_lhota_670.jpeg',
                     title: 'Červená lhota 2022'
                 },
                 {
-                    src: '/img/galerie/2022_sloup.jpg',
+                    src: '/img/galerie/2022_sloup_670.jpg',
                     title: 'Sloup 2022'
                 },
                 {
-                    src: '/img/galerie/2022_svatbaVerca.jpeg',
+                    src: '/img/galerie/2022_svatbaVerca_670.jpeg',
                     title: 'Svatba 2022'
                 },
                 {
-                    src: '/img/galerie/2022_svatosske.jpg',
+                    src: '/img/galerie/2022_svatosske_670.jpg',
                     title: 'Svatošské skály 2022'
                 },
                 {
-                    src: '/img/galerie/2022_zasnuby.jpeg',
+                    src: '/img/galerie/2022_zasnuby_670.jpeg',
                     title: 'Zásnuby 2022'
                 },
                 {
-                    src: '/img/galerie/2022_snezka1.jpeg',
+                    src: '/img/galerie/2022_snezka1_670.jpeg',
                     title: 'Sněžka 2022'
                 },
                 {
-                    src: '/img/galerie/2022_hradek.jpeg',
+                    src: '/img/galerie/2022_hradek_670.jpeg',
                     title: 'Hrádek u Nechanic 2022'
                 }
             ]
--- components/content/MainSlide.vue
+++ components/content/MainSlide.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="main-slide">
-        <img title="test_8c388714fb.jpeg" alt="test_8c388714fb.jpeg" src="/img/header/slide1color.jpg"> 
+        <img title="test_8c388714fb.jpeg" alt="test_8c388714fb.jpeg" src="/img/header/slide1color_1920.jpg" srcset="/img/header/slide1color_500.jpg 600w, /img/header/slide1color_900.jpg 900w, /img/header/slide1color_1200.jpg 1200w, /img/header/slide1color_1920.jpg 1920w, /img/header/slide1color.jpg 2500w"> 
         <!-- <div class="container">
             <div class="main-slider__slide__text">
                 <h1>Hlasujte pro Evropský strom roku 2023</h1>
