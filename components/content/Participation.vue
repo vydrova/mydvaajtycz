e05e484dacef027cb91feb11c8d806fadb9ad8c7
vydrova <57250484+vydrova@users.noreply.github.com>
2023-04-26 16:35:41 +0200

    Účast oprava

--- components/content/Participation.vue
+++ components/content/Participation.vue
@@ -2,7 +2,7 @@
   <section id="ucast">
       <div class="container">
           <h1>Účast</h1>
-          <p>Pokud se svatby plánujete zúčastnit, vyplňte nám prosím následující krátký dotazník <b>minimálně měsíc dopředu</b>.<br>Děkujeme.</p>
+          <p>Pokud se svatby plánujete zúčastnit, vyplňte nám prosím následující krátký dotazník co nejdříve, <b>minimálně měsíc dopředu</b>.<br>Děkujeme.</p>
           <a href="https://forms.gle/R2xrLFStLXw4wHt79" target="_blank" class="button">Vyplnit dotazník</a>
       </div>
   </section>
