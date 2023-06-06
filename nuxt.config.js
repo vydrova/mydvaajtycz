cbab6dfa32ab2264134c88dac7c42daf35cdedcb
vydrova <v.janca@gmail.com>
2023-06-06 20:11:57 +0200

    redirect to ucast

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,6 +80,10 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
+  
+  serverMiddleware: [
+    '~/middleware/redirects.js'
+  ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
